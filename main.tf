b26c9cf94c68733e2f3aaa008d48a6b0e6509ec9
manisha guda <manishavguda@gmail.com>
2023-09-13 16:39:23 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,22 +1,12 @@
-resource "aws_docdb_subnet_group" "default" {
-  name       = "${var.env}-docdb-subnet-group"
-  subnet_ids = var.subnet_ids
-
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-docdb-subnet-group" }
-  )
-}
-
-resource "aws_security_group" "docdb" {
-  name        = "${var.env}-docdb-security-group"
-  description = "${var.env}-docdb-security-group"
+resource "aws_security_group" "main" {
+  name        = "${var.env}-$(var.component)-security-group"
+  description = "${var.env}-$(var.component)-security-group"
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "MongoDB"
-    from_port        = 27017
-    to_port          = 27017
+    description      = "HTTP"
+    from_port        = var.app_port
+    to_port          = var.app_port
     protocol         = "tcp"
     cidr_blocks      = var.allow_cidr
 
@@ -32,33 +22,8 @@ resource "aws_security_group" "docdb" {
 
   tags = merge(
     local.common_tags,
-    { Name = "${var.env}-docdb-security-group" }
+    { Name = "${var.env}-$(var.component)-security-group" }
   )
 }
 
-resource "aws_docdb_cluster" "docdb" {
-  cluster_identifier      = "${var.env}-docdb-cluster"
-  engine                  =
-  engine_version          = var.engine_version
-  master_username         = data.aws_ssm_parameter.DB_ADMIN_USER.value
-  master_password         = data.aws_ssm_parameter.DB_ADMIN_PASS.value
-  skip_final_snapshot     = true
-  db_subnet_group_name    = aws_docdb_subnet_group.default.name
-  vpc_security_group_ids  = [aws_security_group.docdb.id]
-#  storage_encrypted       = true
-#  kms_key_id              = data.aws_kms_key.key.arn
 
-  tags = merge(
-    local.common_tags,
-    { Name = "${var.env}-docdb-cluster" }
-  )
-}
-
-resource "aws_docdb_cluster_instance" "cluster_instances" {
-  count              = var.number_of_instances
-  identifier         = "${var.env}-docdb-cluster-instances-${count.index+1}"
-  cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = var.instance_class
-#  storage_encrypted  = true
-#  kms_key_id         = data.aws_kms_key.key.arn
-}
